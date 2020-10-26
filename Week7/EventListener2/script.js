cffbf9cef39eac025b23edf5d4a1b91aad1fd53f
gellers <gellers22@riverdale.k12.or.us>
2020-10-25 23:03:52 -0700

    Add EventListener2 & 3

--- Week7/EventListener2/script.js
+++ Week7/EventListener2/script.js
@@ -1,18 +1,12 @@
-"use strict"
-window.onload = function() {
-    
-    let container = document.getElementById("container");
-    container.innerHTML = "abc";
+function addElement() {
+    let newDiv = document.createElement("div"); 
+    newDiv.classList.add("box"); 
+
+    newDiv.addEventListener("click", setText);
+    var container = document.getElementById("container");
+    container.appendChild(newDiv); 
+
 }
-var divCount = 0;
-function add() {
-    let container = document.getElementById("container");
-    var n = document.createElement("div");
-    divCount++;
-    n.innerHTML = divCount;
-    n.addEventListener("click", remove);
-    container.appendChild(n);
-}
-function remove() {
-    this.parentElement.removeChild(this); 
+function setText() {
+    this.innerHTML = "This is this!"; 
 }
