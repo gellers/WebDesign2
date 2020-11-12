7fc49f585558a898eb2ed0f0c406c91824519468
gellers <gellers22@riverdale.k12.or.us>
2020-11-12 09:03:29 -0800

    Confused about what assignment actually entails

--- Week10/SavingSettings/script.js
+++ Week10/SavingSettings/script.js
@@ -1,13 +1,40 @@
+function saveInputs() {
+    saveUser(); 
+    saveSettings(); 
+}
 function saveSettings(){
-    //here you need to save the saveSettings
-    //I have called applySettings() for you.
+    //this should be the value of the child node that is selected
+    let backgroundColor = document.getElementById("selBackground"); 
+    let jsonColor = JSON.stringify("color", backgroundColor); 
+    localStorage.setItem("color", jsonColor); 
+
     applySettings();
 }
 function applySettings(){
+    let color = localStorage.getItem("color"); 
+    //test
+    console.log("color: " + color)
     var circles = document.getElementsByClassName("content");
+    //also not working
     for(var circle of circles){
-        //this loops through everything with a class of 'content' and you will apply the settings to each.
-        circle.style.boxShadow = "2px 2px grey";
+        circle.style.backgroundColor = color;
     }
 }
-//Remember, too, to create the functionality to retrieve settings.
+function saveUser() {
+    let txtUser = document.getElementById("txtUser"); 
+    localStorage.setItem("user", JSON.stringify(txtUser.value));  
+    //test
+    console.log("user: " + localStorage.getItem("user"));
+
+     applyUser();
+}
+function applyUser() {
+    let header = document.getElementById("header"); 
+    //not working for some reason?
+    let user = localStorage.getItem("user").parse; 
+    
+    if (user === undefined || user === null) {
+        user = '[user]'; 
+    }
+    header.innerHTML = 'Saving Settings--Hello ' + user; 
+}
