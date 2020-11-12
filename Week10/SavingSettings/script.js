82359043514efb3bdf6601a1ae4fa617d31551f6
gellers <gellers22@riverdale.k12.or.us>
2020-11-12 09:32:47 -0800

    Finsh work (and works)

--- Week10/SavingSettings/script.js
+++ Week10/SavingSettings/script.js
@@ -1,40 +1,36 @@
-function saveInputs() {
-    saveUser(); 
-    saveSettings(); 
-}
-function saveSettings(){
-    //this should be the value of the child node that is selected
-    let backgroundColor = document.getElementById("selBackground"); 
-    let jsonColor = JSON.stringify("color", backgroundColor); 
-    localStorage.setItem("color", jsonColor); 
-
-    applySettings();
-}
-function applySettings(){
-    let color = localStorage.getItem("color"); 
-    //test
-    console.log("color: " + color)
-    var circles = document.getElementsByClassName("content");
-    //also not working
-    for(var circle of circles){
-        circle.style.backgroundColor = color;
-    }
-}
-function saveUser() {
-    let txtUser = document.getElementById("txtUser"); 
-    localStorage.setItem("user", JSON.stringify(txtUser.value));  
-    //test
-    console.log("user: " + localStorage.getItem("user"));
-
-     applyUser();
-}
-function applyUser() {
-    let header = document.getElementById("header"); 
-    //not working for some reason?
-    let user = localStorage.getItem("user").parse; 
-    
-    if (user === undefined || user === null) {
-        user = '[user]'; 
-    }
-    header.innerHTML = 'Saving Settings--Hello ' + user; 
-}
+window.onload = function() {
+    saveInputs(); 
+  }; 
+  function saveInputs() {
+      saveUser(); 
+      saveSettings(); 
+  }
+  function saveSettings(){
+      let backgroundColor = document.getElementById("selBackground").value; 
+      localStorage.setItem("color", backgroundColor); 
+      
+      applySettings();
+  }
+  function applySettings(){
+      let color = localStorage.getItem("color"); 
+      
+      var circles = document.getElementsByClassName("content");
+      for(var circle of circles){
+          circle.style.backgroundColor = color;
+      }
+  }
+  function saveUser() {
+      let txtUser = document.getElementById("txtUser").value; 
+      localStorage.setItem("user", txtUser);  
+  
+       applyUser();
+  }
+  function applyUser() {
+      let header = document.getElementById("header"); 
+      let user = localStorage.getItem("user"); 
+      
+      if (user === null) {
+          user = '[user]'; 
+      }
+      header.innerHTML = 'Saving Settings--Hello ' + user; 
+  }
